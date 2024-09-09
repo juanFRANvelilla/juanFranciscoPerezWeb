be606e3ea13ff349f5e28f4855b40ccb98a99bcb
juanFRANvelilla <juanfran01velilla@gmail.com>
2024-09-09 09:21:55 +0200

    cambios en curriculum

--- static/js/downloadCV.js
+++ static/js/downloadCV.js
@@ -1,4 +1,4 @@
-document.getElementById('download-cv-spanish').addEventListener('click', function() {
+document.getElementById('download-cv').addEventListener('click', function() {
     const link = document.createElement('a');
     link.href = 'static/cv/JuanFranciscoPerez CV.pdf'; 
     link.download = 'JuanFranciscoPerez CV.pdf'; 
@@ -8,16 +8,3 @@ document.getElementById('download-cv-spanish').addEventListener('click', functio
     document.body.removeChild(link); 
 });
 
-
-
-
-
-document.getElementById('download-cv-english').addEventListener('click', function() {
-    const link = document.createElement('a');
-    link.href = 'static/cv/JuanFranciscoPerez Resume.pdf'; 
-    link.download = 'JuanFranciscoPerez Resume.pdf'; 
-    link.style.display = 'none'; 
-    document.body.appendChild(link);
-    link.click(); 
-    document.body.removeChild(link); 
-});
